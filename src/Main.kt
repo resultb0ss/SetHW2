115e907dbef6fe769ba8012b024e5d5ab8e92bdb
Admin <reteamboss@gmail.com>
2024-09-23 17:06:13 +0300

    2

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,5 @@
 fun main() {
-    println("Hello World!")
+    var mySet = setOf(1,2,3,4,5,6,7,8,9,10,15,20,13,14,34)
+    var newSet = mySet.map { it * 2 }.filter { it % 5 == 0 }
+    println(newSet)
 }
